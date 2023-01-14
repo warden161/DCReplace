7a283e4616124fd98ec088c923103bde6db00e9e
TheUltiOne <118567564+warden161@users.noreply.github.com>
2023-01-14 16:15:23 -0500

    Fix version

--- Plugin.cs
+++ Plugin.cs
@@ -6,7 +6,7 @@ namespace DCReplace
     {
         public override string Name { get; } = "DCReplace";
         public override string Author { get; } = "warden161";
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 1, 0);
         public override Version RequiredExiledVersion { get; } = new Version(6, 0, 0);
 
         public static Plugin Instance { get; private set; }
